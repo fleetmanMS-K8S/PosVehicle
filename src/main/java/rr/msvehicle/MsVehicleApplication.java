c3b93751813cd016a2541b2c2e5b0c506a33b06f
manish978973 <manish.rama@ovgu.de>
2020-09-20 21:50:57 +0200

    Second commit

--- src/main/java/rr/msvehicle/MsVehicleApplication.java
+++ src/main/java/rr/msvehicle/MsVehicleApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
@@ -12,6 +13,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableRabbit
+@EnableHystrixDashboard
 public class MsVehicleApplication {
 
 
