4e0acd8fc294059e815e26986c0c2160cf743d5a
manish978973 <manish.rama@ovgu.de>
2020-10-09 15:27:01 +0200

    First commit for K8S deployment

--- dockerfile
+++ dockerfile
@@ -1,3 +1,3 @@
 FROM openjdk:8-jdk-alpine
-COPY ms-vehicle-0.0.1-SNAPSHOT.jar trackerfetch.jar
+COPY target/ms-vehicle-0.0.1-SNAPSHOT.jar trackerfetch.jar
 ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","trackerfetch.jar"]
--- src/main/java/rr/msvehicle/Remotecall.java
+++ src/main/java/rr/msvehicle/Remotecall.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name="fleetman-tracker")
+@FeignClient(name="fleetman-tracker", url="http://miniposra:8888")
 public interface Remotecall {
 
         @RequestMapping(method= RequestMethod.GET, value="/position/fetch/vehicle")
